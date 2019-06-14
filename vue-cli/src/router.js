d6c437e309f6c97b6dbc8b9be4ab1768e8107e02
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-14 14:16:46 -0700

    added info into router.js and main.js...added info about of the pages so they can navigaet

---
+++ vue-cli/src/router.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import Router from "vue-router";
+import home from "./pages/home.vue";
+import photo from "./pages/photo.vue";
+import prices from "./pages/prices.vue";
+import things from "./pages/things.vue";
+import weather from "./pages/weather.vue";
+
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/home",
+      name: "home",
+      component: home
+    },
+    {
+      path: "/photo",
+      name: "photo",
+      component: photo
+    },
+    {
+      path: "/prices",
+      name: "prices",
+      component: prices
+    },
+    {
+      path: "/things",
+      name: "things",
+      component: things
+    },
+    {
+      path: "/weather",
+      name: "weather",
+      component: weather
+    }
+
+  ]
+});
