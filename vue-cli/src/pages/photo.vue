76a2149f609f70649b14f9684fe34f38177062c2
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-15 19:36:06 -0700

    new image added, messing around with background on home page

--- vue-cli/src/pages/photo.vue
+++ vue-cli/src/pages/photo.vue
@@ -10,7 +10,7 @@
 </template>
 <script>
   import axios from 'axios'
-
+  
 
   export default {
     name: 'photo',
@@ -18,25 +18,11 @@
       return {
         title: "Photo Page",
         info: null,
-        listings: [],
         AuthStr: '563492ad6f9170000100000133f51eab663d4ca49c54c50c4e39bc80',
         url: 'https://api.pexels.com/v1/search?query=example+query&per_page=15&page=1'
       }
     },
-    created() {
-      axios.get(`url`, {
-          'headers': {
-            'Authorization': data().AuthStr
-          }
-        })
-        .then((response => {
-            this.listings = response.data;
-          })
-          .catch((error) => {
-            console.log(error)
-          })
-        }
-    }
+
   }
 
 </script>
