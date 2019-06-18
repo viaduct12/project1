e76baf71ddd911cb45960b1d0bd34262670a5b35
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-18 10:46:31 -0700

    created deploy.sh file, took out /project path in webpack.config.js

---
+++ vue-cli/deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://viaduct12.github.io
+# git push -f git@github.com:viaduct12/vidaduct12.github.io.git master
+
+# if you are deploying to https://viaduct12.github.io/project1
+# git push -f git@github.com:viaduct12/project1.git master:gh-pages
+
+cd -
