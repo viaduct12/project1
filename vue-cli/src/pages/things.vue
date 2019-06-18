e9add99d4948f8ab22b8335c285fcd063ae5b524
Lawrence Fiesta <lfiesta86@gmail.com>
2019-06-17 18:50:43 -0700

    got the youtube video to embed working on displaying more than just one

--- vue-cli/src/pages/things.vue
+++ vue-cli/src/pages/things.vue
@@ -1,20 +1,27 @@
-<template>
-  <div id="things">
 
-  <h1>{{info}}</h1>
+<template>
+  <div id="app">
+    {{info}}
     
+    <youtube :video-id="videoId"></youtube>
+
   </div>
 </template>
-<script>
 
+<script>
+import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VueYouTubeEmbed from 'vue-youtube-embed'
+
+Vue.use(VueYouTubeEmbed)
 
 export default{
-  name: "things",
+  name: "app",
   data () {
     return {
-      videoId : [],
+      videoIds : [],
+      videoId:'videoId',
       info: "not working help"
     }
   },
@@ -28,19 +35,44 @@ export default{
     }
   })
   .then(response => {
-      
       var results = response;
       console.log(results);
-      
-        for (var i = 0; i < 4; i++) {      
-          this.videoId.push("https://www.youtube.com/watch?v=" + results.data.items[i].id.videoId);
-
+        for (var i = 0; i < 4; i++) {
+          // this.videoIds.push("https://www.youtube.com/watch?v=" + results.data.items[i].id.videoId);
+          this.videoIds.push(results.data.items[i].id.videoId);
         }
+
+        this.videoId = this.videoIds.pop();      
     })
   }
 }
 </script>
-<style>
 
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
-
