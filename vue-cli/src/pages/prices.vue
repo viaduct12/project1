21e3389f2a2f9c0b4f181031fd3d2185ef1724ad
ninateng <niinateng@gmail.com>
2019-06-19 18:56:49 -0700

    map on prices is in progress , working multiple youtube videos

--- vue-cli/src/pages/prices.vue
+++ vue-cli/src/pages/prices.vue
@@ -1,23 +1,36 @@
 <template>
-  <div id="prices">
+  <div id="container">
+    <div id="prices-container">
+      <h1>{{msg}}</h1>
+      <h1>{{priceDisplay}}</h1>
+    </div>
 
-  <h1>{{msg}}</h1>
-  <h1>{{priceDisplay}}</h1>
+    <!-- spacer div -->
+    <div id="spacer"></div>
 
+    <!-- google map -->
+    <div class="google-map" :id="mapName">
+      <GmapMap
+        :center="{lat:40.7127753, lng:-74.0059728}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 500px; height: 300px"
+      ></GmapMap>
+    </div>
+  <!-- google map -->
 
-    
   </div>
 </template>
 <script>
-  import axios from 'axios';
-  import VueAxios from 'vue-axios';
-  import $ from "jquery";
-  // import travels from "../components/travels.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+//importing google-map
+import GoogleMapsLoader from "google-maps";
 
 export default {
-  name: 'prices',
-
-  data () {
+  name: "prices",
+  props: ["name"],
+  data() {
     return {
       msg: "Prices Page",
       price: [],
@@ -28,172 +41,86 @@ export default {
       destination: this.$store.getters.flightInfo.destinationCode, //user input LAX
       departing: this.$store.getters.flightInfo.dateCode, //user input 2019-09-01
       // return: "2019-09-15", //user input
-      city: ""
-    }
+      city: "",
+      mapName: this.name + "-map"
+    };
   },
+
+
   mounted() {
-     
+    getMap();
+    // google map 
+    function getMap(){
+
+    
+    const element = document.getElementById(this.mapName);
+    const options = {
+      zoom: 14,
+      center: new google.maps.LatLng(40.7127753, -74.0059728)
+    };
+    const map = new google.maps.Map(element, options);
+    };
+    //google map 
+
     axios({
-       method: "GET",
-       url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" + this.origin +"-sky/"+ this.destination + "-sky/" + this.departing,
-       headers: {
-        'X-RapidAPI-Host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
+      method: "GET",
+      url:
+        "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/" +
+        this.origin +
+        "-sky/" +
+        this.destination +
+        "-sky/" +
+        this.departing,
+      headers: {
+        "X-RapidAPI-Host":
+          "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
         "X-RapidAPI-Key": "ec7228ea2fmsh9b2bb9e23723a32p1f2123jsnb1ea8ec75a54"
-       },
+      }
       //  params: {
       //   inboundPartialDate: this.return
       //  }
-     })
-    .then(response => {
+    }).then(response => {
       console.log(response);
 
       this.city = response.data.Places[0].CityName;
       this.$store.commit({
-        type: 'changeCity', 
-        
+        type: "changeCity",
+
         newCity: this.city
       });
-    
 
-      for(var i = 0; i < response.data.Quotes.length; i++){
+      for (var i = 0; i < response.data.Quotes.length; i++) {
         this.carrierID.push(response.data.Quotes[i].OutboundLeg.CarrierIds[0]);
-        this.price.push(response.data.Quotes[i].MinPrice);       
+        this.price.push(response.data.Quotes[i].MinPrice);
       }
 
-      // console.log(this.carrierID.length); 
-      for(var j = response.data.Carriers.length - 1; j >= 0 ; j--){
-          for(var k = 0; k < this.carrierID.length; k++){
-            if(this.carrierID[k] === response.data.Carriers[j].CarrierId){
-              this.airline.push(response.data.Carriers[j].Name);
-            }
+      // console.log(this.carrierID.length);
+      for (var j = response.data.Carriers.length - 1; j >= 0; j--) {
+        for (var k = 0; k < this.carrierID.length; k++) {
+          if (this.carrierID[k] === response.data.Carriers[j].CarrierId) {
+            this.airline.push(response.data.Carriers[j].Name);
           }
-      };         
-      //displays prices + airlines  
-      for(var i = 0; i < this.price.length; i++){
+        }
+      }
+      //displays prices + airlines
+      for (var i = 0; i < this.price.length; i++) {
         this.priceDisplay = "$" + this.price[i] + " " + this.airline[i];
         console.log(this.priceDisplay, "whaaaat?!");
-      };
-
-   })
-
-
-
-     
+      }
+    });
   }
-
-
-
-
-
-//====== axios shitbag     
-    //  axios({
-    //   method: "POST",
-    //   url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0", 
-    //   data: {
-    //       cabinClass: "business",
-    //       country: "US",
-    //       currency: "USD",
-    //       locale: "en-US",
-    //       originPlace: "SEA-sky",
-    //       destinationPlace: "LAX-sky",
-    //       inboundDate: "2019-09-10",
-    //       outboundDate: "2019-09-01",
-    //       infants: "0",
-    //       children: "0",
-    //       adults: "1"
-    //     }, 
-    //   headers: {
-    //     'cache-control': 'private',
-        // 'X-RapidAPI-Host': 'skyscanner-skyscanner-flight-search-v1.p.rapidapi.com',
-        // "X-RapidAPI-Key": "ec7228ea2fmsh9b2bb9e23723a32p1f2123jsnb1ea8ec75a54",
-    //     "Content-Type": "application/x-www-form-urlencoded"
-    //     },
-    //   withCredentials: true,
-    //   })
-    //   .then(function(response, error, cool){
-    //     console.log(response, error, cool);
-    //   }).catch(err=>{
-    //     throw err;
-    //   });
-    // }   
-
-
-
-
-//=== ajax shit bag    
-    // console.log("before crap ");
-    // var crap = getStuffz();
-    // console.log("what is crap? " + crap);
-    // this.price = getStuffz(); 
-      
-    //   function getStuffz(){
-    //   $.ajax({
-    //   method: "POST",
-    //   url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/v1.0",
-    //   headers: {
-    //     'cache-control': 'private',
-    //     "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    //     // 'X-RapidAPI-Key': "ec7228ea2fmsh9b2bb9e23723a32p1f2123jsnb1ea8ec75a54",
-    //     "X-RapidAPI-Key": "e52d2aae21mshab7235480ad785cp1f2bb4jsn5ee55b97d844",
-    //     "Content-Type": "application/x-www-form-urlencoded"
-    //   },
-    //   data: {
-    //     // "cabinClass": "business, premium ", 
-    //     "country": "US",
-    //     "currency": "USD",
-    //     "locale": "en-US",
-    //     "originPlace": "SFO-sky",
-    //     "destinationPlace": "LHR-sky",
-    //     "inboundDate": "2019-09-10",
-    //     "outboundDate": "2019-09-01",
-    //     "infants": "0",
-    //     "children": "0",
-    //     "adults": "1"
-    //   },
-    //   success: function(data, textStatus, request){
-    //     var locationHeader = request.getResponseHeader("Location");
-    //     var locationHeaderArray = [];
-    //     locationHeaderArray = locationHeader.split("/");
-    //     console.log(locationHeader, locationHeaderArray);
-    //     var sessionKey = locationHeaderArray[locationHeaderArray.length - 1];
-    //     console.log(sessionKey, " sessionKey");
-    //     return arghhImDying(sessionKey);
-    //   },
-    //   error: function (request, textStatus, errorThrown) {
-    //     alert(request.getResponseHeader('Location'));
-    //   }
-    // })}
-    // function arghhImDying(key){
-    //   var price;
-    //   $.ajax({
-    //     method:"GET",
-    //     url: "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/pricing/uk2/v1.0/" + key + "?",
-    //     headers:{
-    //       "X-RapidAPI-Host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    //       // "X-RapidAPI-Key": "ec7228ea2fmsh9b2bb9e23723a32p1f2123jsnb1ea8ec75a54"
-    //       "X-RapidAPI-Key": "e52d2aae21mshab7235480ad785cp1f2bb4jsn5ee55b97d844"
-    //     },
-    //     data: {
-    //       "sortType" : "price",
-    //       "sortOrder" : "asc",
-    //       "pageIndex" : 0,
-    //       "pageSize" : 5
-    //     }
-    //   }).then(function(response){
-    //     console.log(response);
-    //     console.log(response.Itineraries[0].PricingOptions[0].Price);
-    //     return "$" + response.Itineraries[0].PricingOptions[0].Price;
-    //     this.data.price = $(this).price;
-    //     console.log(this.price, this.data.price); 
-    //     this.msg = this.price;
-    //   })
-    //   console.log("outside of the promise", price);
-    // }
-    
-  // }
-}
+};
 </script>
-<style>
-
+<style scoped>
+/* google map */
+.google-map {
+  width: 475px;
+  height: 275px;
+  margin: 0 auto;
+  margin-right: 10px;
+  background: gray;
+  border-radius: 25px;
+}
+/* google map */
 </style>
 
--- vue-cli/src/pages/things.vue
+++ vue-cli/src/pages/things.vue
@@ -2,60 +2,103 @@
 <template>
   <div id="app">
     {{info}}
-
-    <!-- <div v-for="index in videoIds">     -->
-    <youtube :video-id="videoId"></youtube>
-    <!-- <youtube :video-id="videoId"></youtube> -->
-    <!-- <youtube :video-id="videoId"></youtube> -->
-    <!-- <youtube :video-id="videoId"></youtube> -->
+    <!-- <div id="video-Container"> -->
+      <!-- <div v-for="index in videoIds">     -->
+      <!-- <youtube :video-id="videoId1"></youtube>
+      <youtube :video-id="videoId2"></youtube>
+      <youtube :video-id="videoId3"></youtube>
+      <youtube :video-id="videoId4"></youtube> -->
+      <!-- <youtube :video-id="videoId"></youtube> -->
+      <!-- <youtube :video-id="videoId"></youtube> -->
+      <!-- <youtube :video-id="videoId"></youtube> -->
+      <!-- </div> -->
     <!-- </div> -->
+
+    <div class="row">
+      <div class="column">
+        <youtube :video-id="videoId1"></youtube>
+      </div>
+
+      <!-- div space left and right videos apart -->
+      <div class="column" id=" space-LR-videos"></div>
+
+      <div class="column">
+        <youtube :video-id="videoId2"></youtube>
+      </div>
+      
+    </div>
+
+  <div id="space-videos"></div>
+
+
+    <div class="row">
+      <div class="column">
+        <youtube :video-id="videoId3"></youtube>
+      </div>
+
+      <!-- div space left and right videos apart -->
+      <div class="column" id=" space-LR-videos"></div>
+
+      <div class="column">
+        <youtube :video-id="videoId4"></youtube>
+      </div>
+    </div>
+
+    
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import VueYouTubeEmbed from 'vue-youtube-embed'
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import VueYouTubeEmbed from "vue-youtube-embed";
 
-Vue.use(VueYouTubeEmbed)
+Vue.use(VueYouTubeEmbed);
 
-export default{
+export default {
   name: "app",
-  data () {
+  data() {
     return {
-      videoIds : [],
-      videoId:'videoId',
-      info: "not working help",
+      videoIds: [],
+      videoId1: "videoId",
+      videoId2: "",
+      vvideoId3: "",
+      videoId4: "",
+      info: "Suggested Videos to Assist Your Travels",
       queryCountry: this.$store.getters.city //this.city
-    }
+    };
   },
-  mounted (){
+  mounted() {
     console.log(this.queryCountry, "my cityyyyyy!");
-    axios.get('https://www.googleapis.com/youtube/v3/search', {
-      params: {
-      part: "snippet, id",
-      q: "travel guide " + this.queryCountry,
-      key: "AIzaSyBGnNxm3xCX-HWVQrxhPk-dfEiZAOx_FVQ"
-    }
-  })
-  .then(response => {
-      var results = response;
-      console.log(results);
+    axios
+      .get("https://www.googleapis.com/youtube/v3/search", {
+        params: {
+          part: "snippet, id",
+          q: "travel guide " + this.queryCountry,
+          key: "AIzaSyBGnNxm3xCX-HWVQrxhPk-dfEiZAOx_FVQ"
+        }
+      })
+      .then(response => {
+        var results = response;
+        console.log(results);
         for (var i = 0; i < 4; i++) {
           // this.videoIds.push("https://www.youtube.com/watch?v=" + results.data.items[i].id.videoId);
           this.videoIds.push(results.data.items[i].id.videoId);
         }
 
-        this.videoId = this.videoIds[0];      
-    })
+        this.videoId1 = this.videoIds[0];
+        this.videoId2 = this.videoIds[1];
+        this.videoId3 = this.videoIds[2];
+        this.videoId4 = this.videoIds[3];
+      });
   }
-}
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -63,7 +106,8 @@ export default{
   margin-top: 60px;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -80,4 +124,38 @@ li {
 a {
   color: #42b983;
 }
+
+// #video-container 
+//   height: 400px;
+//   width: 400px;
+// 
+
+* {
+  box-sizing: border-box;
+}
+
+.column {
+  float: left;
+  width: 33.33%;
+  padding: 5px;
+}
+
+/* Clearfix (clear floats) */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+#space-videos {
+  width: 10px;
+  height: 50%;
+}
+
+#space-LR-videos {
+  width: 1px;
+
+}
+
+
 </style>
